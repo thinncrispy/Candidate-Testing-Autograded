b78b01988816a1617779ac49cd5b7a9cab9dce87
thinncrispy <wilson.hayward@gmail.com>
2024-01-11 18:02:56 -0600

    tests 1-6 passed!

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
@@ -18,11 +18,14 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+const input = require('readline-sync');
+let candidateName = input.question("What is your name?")
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+const input = require('readline-sync');
+let candidateAnswer = input.question("question");
 
 
 }
@@ -42,7 +45,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+   console.log("Welcome to the quiz!" + candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
